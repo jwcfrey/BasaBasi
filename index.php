3b76d737553fa46715f613b3225e1b1f5272d867
jwcfrey <bondanbanuaji@gmail.com>
2024-12-28 11:57:04 +0700

    Finally, BasaBasi Realtime Project DONE!!!

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BasaBasi Apps</title>
+    <link rel="icon" href="./src/image/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./src/style/index.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo"><img src="./src/image/logo.png" alt="logo"></a>
+    </header>
+
+    <section class="home">
+        <div class="home-content">
+            <h1><header>Basa<span id="basiSpan"></span></header></h1>
+            <h3>Ngobrol Tanpa Batas, Kapan Saja, Di Mana Saja</h3>
+            <p>BasaBasi hadir untuk kaum Gen-Z yang nggak mau kehilangan momen seru dalam obrolan bareng temen, sahabat, apalagi doi. Chat realtime yang cepat dan mudah, jadi setiap percakapan tetap nyambung tanpa gangguan. Ayo, mulai ngobrol sekarang dan rasakan kenyamanan chat tanpa batas!"</p>
+            <div class="btn-box">
+                <a href="signup.php">Daftar</a>
+                <a href="login.php">Masuk</a>
+            </div>
+        </div>
+        <div class="home-sci">
+            <a href=""><i class="fab fa-google-drive"></i></a>
+            <a href=""><i class='bx bxl-github'></i></a>
+            <a href=""><i class='bx bxl-youtube'></i></a>
+        </div>
+        <span class="home-img"></span>
+    </section>
+
+    <script src="./src/js/circleCursor.js"></script>
+    <script src="./src/js/typingAnimation.js"></script>    
+</body>
+</html>
